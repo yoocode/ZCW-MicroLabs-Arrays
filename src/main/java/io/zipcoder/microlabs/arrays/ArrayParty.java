af9990973ee5fc96443416c640c2e7cc4a9bb674
Byung-Chun Yoo <byung-chunyoo@wifi-roaming-128-4-196-21.host.udel.edu>
2017-05-10 22:01:18 -0400

    upload pring array

--- src/main/java/io/zipcoder/microlabs/arrays/ArrayParty.java
+++ src/main/java/io/zipcoder/microlabs/arrays/ArrayParty.java
@@ -3,9 +3,24 @@ package io.zipcoder.microlabs.arrays;
 
 public class ArrayParty {
 
-    public String printArray(String[] inputArray){
-        return "";
+    public String printArray(String[] inputArray) {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("*** Output ***\n");
+
+        for (int i = 0; i < inputArray.length; i++) {
+            if (i > 0){
+                sb.append("\n");
+            }
+            String item = inputArray[i];
+            sb.append(item);
+        }
+        return sb.toString();
     }
+
+
+
 
     public String lastElement(String[] inputArray){
         return "";
